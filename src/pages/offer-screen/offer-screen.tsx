b5b3e4e83abd88c6786bc5ac57cf185ae9bda838
slavalinwood <slavalinwood@gmail.com>
2025-05-15 13:57:08 +0300

    refactor(offer-screen.tsx): создаёт и использует компоненты ревью и списка ревью

--- src/pages/offer-screen/offer-screen.tsx
+++ src/pages/offer-screen/offer-screen.tsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import { Offers } from '../../types/offer';
 import NotFoundScreen from '../not-found-screen/not-found-screen';
 import ReviewForm from '../../components/review-form';
+import ReviewsList from '../../components/reviews-list';
 
 type OfferScreenProps = {
   offers: Offers;
@@ -162,37 +163,7 @@ function OfferScreen({offers}: OfferScreenProps): JSX.Element {
               </div>
               <section className="offer__reviews reviews">
                 <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{offer.reviews.length}</span></h2>
-                <ul className="reviews__list">
-                  {
-                    offer.reviews.map((e, i) => {
-                      const keyValue = `${i}-${e.comment}`;
-                      return (
-                        <li key={keyValue} className="reviews__item">
-                          <div className="reviews__user user">
-                            <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                              <img className="reviews__avatar user__avatar" src={e.user.avatar} width="54" height="54" alt="Reviews avatar"/>
-                            </div>
-                            <span className="reviews__user-name">
-                              {e.user.name}
-                            </span>
-                          </div>
-                          <div className="reviews__info">
-                            <div className="reviews__rating rating">
-                              <div className="reviews__stars rating__stars">
-                                <span style={{width: `${20 * e.stars}%`}}></span>
-                                <span className="visually-hidden">Rating</span>
-                              </div>
-                            </div>
-                            <p className="reviews__text">
-                              {e.comment}
-                            </p>
-                            <time className="reviews__time" dateTime="2019-04-24">{e.date}</time>
-                          </div>
-                        </li>
-                      );
-                    })
-                  }
-                </ul>
+                <ReviewsList reviews={offer.reviews} />
                 <ReviewForm />
               </section>
             </div>
