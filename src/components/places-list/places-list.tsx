67fbfaa6feb13833b09205b29112026342f4e1c7
slavalinwood <slavalinwood@gmail.com>
2025-05-16 13:54:41 +0300

    refactor(places-list.tsx): избавляется от стейта компонента

--- src/components/places-list/places-list.tsx
+++ src/components/places-list/places-list.tsx
@@ -1,6 +1,5 @@
 import PlaceCard from '../place-card/place-card';
 import { Offers } from '../../types/offer';
-import { useState } from 'react';
 
 type PlacesListProps = {
   offers: Offers;
@@ -8,15 +7,11 @@ type PlacesListProps = {
 }
 
 function PlacesList({offers, getActiveCardId}: PlacesListProps): JSX.Element {
-  const [activeCardId, setActiveCardId] = useState(0);
-
-  getActiveCardId(activeCardId);
-
   return (
     <div className='cities__places-list places__list tabs__content'>
       {offers.map((e) => {
         const keyValue = e.id;
-        return <PlaceCard key={keyValue} offer={e} onActiveCard={() => setActiveCardId(e.id)} onNoActiveCard={() => setActiveCardId(0)}/>;
+        return <PlaceCard key={keyValue} offer={e} onActiveCard={() => getActiveCardId(e.id)} onNoActiveCard={() => getActiveCardId(0)}/>;
       })}
     </div>
   );
