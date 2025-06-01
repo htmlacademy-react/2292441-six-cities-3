ed1b2f437958c16cf1f077d5e1008475f97fb7fd
slavalinwood <slavalinwood@gmail.com>
2025-06-02 00:45:15 +0300

    refactor(city-tabs): выносит логику события клика в функцию

--- src/components/city-tabs/city-tabs.tsx
+++ src/components/city-tabs/city-tabs.tsx
@@ -1,11 +1,19 @@
+import { MouseEvent } from 'react';
 import { CITIES } from '../../const';
 import { useAppDispatch } from '../../hooks/use-app-dispatch';
 import { useAppSelector } from '../../hooks/use-app-selector';
 import { fillPlacesList, setCity } from '../../store/action';
+import { City } from '../../types/city';
 
 function CityTabs(): JSX.Element {
   const currentCity = useAppSelector((state) => state.city);
   const dispatch = useAppDispatch();
+
+  const cityChangeHandler = (evt: MouseEvent, city: City) => {
+    evt.preventDefault();
+    dispatch(setCity(city));
+    dispatch(fillPlacesList(city.name));
+  };
 
   return (
     <div className="tabs">
@@ -14,11 +22,7 @@ function CityTabs(): JSX.Element {
           {CITIES.map((city) => (
             <li className="locations__item" key={city.name}>
               <a className={`locations__item-link tabs__item ${(city.name === currentCity.name) ? 'tabs__item--active' : ''}`}
-                onClick={(evt) => {
-                  evt.preventDefault();
-                  dispatch(setCity(city));
-                  dispatch(fillPlacesList(city.name));
-                }}
+                onClick={(evt) => cityChangeHandler(evt, city)}
               >
                 <span>{city.name}</span>
               </a>
