2981c029828863f622af969d3ea6e697b373ffa2
slavalinwood <slavalinwood@gmail.com>
2025-06-23 21:24:26 +0300

    создаёт действия логина и проверки авторизации

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,6 +1,7 @@
 import { createAction } from '@reduxjs/toolkit';
 import { City, CityName } from '../types/city';
 import { Offer } from '../types/offer';
+import { AuthorizationStatus } from '../const';
 
 export const setCity = createAction<City>('main/setCity');
 
@@ -9,3 +10,5 @@ export const fillPlacesList = createAction<CityName>('main/fillPlacesList');
 export const setCurrentOffer = createAction<Offer>('offer/setCurrentOffer');
 
 export const setActiveOfferId = createAction<string>('offer/setActiveOfferId');
+
+export const setAuthorizationStatus = createAction<AuthorizationStatus>('authorization/setAuthorizationStatus');
--- src/store/api-action.ts
+++ src/store/api-action.ts
@@ -2,8 +2,11 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { AppDispatch, State } from '../types/state';
 import { AxiosInstance } from 'axios';
 import { Offers } from '../types/offer';
-import { APIRoute, DEFAULT_CITY } from '../const';
-import { fillPlacesList } from './action';
+import { APIRoute, AuthorizationStatus, DEFAULT_CITY } from '../const';
+import { fillPlacesList, setAuthorizationStatus } from './action';
+import { AuthData } from '../types/auth-data';
+import { UserData } from '../types/user-data';
+import { saveToken } from '../services/token';
 
 export const fetchOffers = createAsyncThunk<Offers, undefined, {
   dispatch: AppDispatch;
@@ -15,5 +18,34 @@ export const fetchOffers = createAsyncThunk<Offers, undefined, {
     const { data } = await api.get<Offers>(APIRoute.Offers);
     dispatch(fillPlacesList(DEFAULT_CITY));
     return data;
+  }
+);
+
+export const checkAuth = createAsyncThunk<void, undefined, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/checkAuth',
+  async (_arg, {dispatch, extra: api}) => {
+    try {
+      await api.get(APIRoute.Login);
+      dispatch(setAuthorizationStatus(AuthorizationStatus.Auth));
+    } catch {
+      dispatch(setAuthorizationStatus(AuthorizationStatus.NoAuth));
+    }
+  }
+);
+
+export const login = createAsyncThunk<void, AuthData, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/login',
+  async ({login: email, password}, {dispatch, extra: api}) => {
+    const {data: {token}} = await api.post<UserData>(APIRoute.Login, {email, password});
+    saveToken(token);
+    dispatch(setAuthorizationStatus(AuthorizationStatus.Auth));
   }
 );
