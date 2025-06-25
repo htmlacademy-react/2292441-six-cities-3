98d3f74c3abd4b2575c8633cb1459120cc21b5a4
slavalinwood <slavalinwood@gmail.com>
2025-06-25 04:14:37 +0300

    feat(login-screen): реализует авторизацию

--- src/pages/login-screen/login-screen.tsx
+++ src/pages/login-screen/login-screen.tsx
@@ -1,17 +1,56 @@
+import { FormEvent, useRef } from 'react';
+import { useAppDispatch } from '../../hooks/use-app-dispatch';
+import { login } from '../../store/api-action';
+
 function LoginScreen(): JSX.Element {
+  const loginRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const dispatch = useAppDispatch();
+
+  const submitHandler = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if (loginRef.current !== null && passwordRef.current !== null) {
+      dispatch(login({
+        login: loginRef.current.value,
+        password: passwordRef.current.value
+      }));
+    }
+  };
+
   return (
     <main className="page__main page__main--login">
       <div className="page__login-container container">
         <section className="login">
           <h1 className="login__title">Sign in</h1>
-          <form className="login__form form" action="#" method="post">
+          <form
+            className="login__form form"
+            action="#"
+            method="post"
+            onSubmit={submitHandler}
+          >
             <div className="login__input-wrapper form__input-wrapper">
               <label className="visually-hidden">E-mail</label>
-              <input className="login__input form__input" type="email" name="email" placeholder="Email" required/>
+              <input
+                ref={loginRef}
+                className="login__input form__input"
+                type="email"
+                name="email"
+                placeholder="Email"
+                required
+              />
             </div>
             <div className="login__input-wrapper form__input-wrapper">
               <label className="visually-hidden">Password</label>
-              <input className="login__input form__input" type="password" name="password" placeholder="Password" required/>
+              <input
+                ref={passwordRef}
+                className="login__input form__input"
+                type="password"
+                name="password"
+                placeholder="Password"
+                required
+              />
             </div>
             <button className="login__submit form__submit button" type="submit">Sign in</button>
           </form>
