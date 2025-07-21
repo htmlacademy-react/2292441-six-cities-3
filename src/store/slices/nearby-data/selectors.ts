4a698597ce5b0065451aa68d0a59fd4d70f6fe73
slavalinwood <slavalinwood@gmail.com>
2025-07-21 15:15:14 +0300

    refactor(store): удаляет неиспользуемые селекторы

--- src/store/slices/nearby-data/selectors.ts
+++ src/store/slices/nearby-data/selectors.ts
@@ -2,4 +2,3 @@ import { NameSpace } from '../../../const';
 import { State } from '../../../types/state';
 
 export const selectNearbyOffers = (state: State) => state[NameSpace.Nearby].nearbyOffers;
-export const selectNearbyRequestStatus = (state: State) => state[NameSpace.Nearby].requestStatus;
--- src/store/slices/reviews-data/selectors.ts
+++ src/store/slices/reviews-data/selectors.ts
@@ -2,5 +2,4 @@ import { State } from '../../../types/state';
 import { NameSpace } from '../../../const';
 
 export const selectReviews = (state: State) => state[NameSpace.Reviews].reviews;
-export const selectReviewsRequestStatus = (state: State) => state[NameSpace.Reviews].requestStatus;
 export const selectReviewsPostStatus = (state: State) => state[NameSpace.Reviews].postStatus;
